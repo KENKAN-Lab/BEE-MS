dcb63eb2281473416ee355280971936b7e3427f4
Hisashi Miura <hisashi.office@gmail.com>
2021-06-12 23:52:52 +0900

    試しにdocclass設定を変更

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,6 +43,10 @@ templates_path = ['_templates']
 # Usually you set "language" from the command line for these cases.
 language = 'ja'
 
+# LaTeX の docclass 設定
+latex_docclass = {'manual': 'jsbook'}
+
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
